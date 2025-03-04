725ef89398f63034cd024bfb170a829f3bdd35f8
JHAndrade27 <jhandrade2715@gmail.com>
2025-03-03 22:09:08 -0300

    adicionando css e corrigindo os erros do JS e HTML

--- src/script.js
+++ src/script.js
@@ -1,9 +1,15 @@
 function calcularIdade() {
     let anoDigitado = document.getElementById("anoDeNascimento").value;
+    if (anoDigitado == "") {
+        alert("Por favor, digite o ano de nascimento");
+        return;
+    }
+
+    if (2025 - anoDigitado > 125){
+        alert ("Idade inválida, Digite um ano de nascimento válido, maior que 1899");
+        return;
+    }
 
     let idade = 2025 - anoDigitado;
-
-    console.log(idade);
-
-    document.getElementById("resultado").innerHTML `idade é ${idade}`;
+    document.getElementById("resultado").innerHTML = `idade é ${idade}`;
 }
